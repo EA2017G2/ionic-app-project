ef022d87a4b70feab47f29d80c4f0a25ecf24928
Paula A <paula.urquizo@gmail.com>
2017-12-31 18:18:59 +0100

    Register funcionando.

--- src/pages/register/register.component.ts
+++ src/pages/register/register.component.ts
@@ -4,6 +4,9 @@ import {PlayPage} from "../play/play";
 import { Facebook } from '@ionic-native/facebook';
 import { UserService} from '../services/user.service';
 import {User} from '../../../../ionic-app-project/src/pages/user';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import {TabsPage} from "../tabs/tabs";
+
 @Component({
   selector: 'page-register',
   templateUrl: 'register.component.html'
@@ -11,15 +14,44 @@ import {User} from '../../../../ionic-app-project/src/pages/user';
 export class RegisterPage {
   user: User;
   showUser: boolean = false;
+  regisForm: FormGroup;
 
-  constructor(public navCtrl: NavController, private userService: UserService, private facebook: Facebook) {
+  sex = [
+    'Male', 'Female'];
 
+
+  constructor(public navCtrl: NavController, private userService: UserService, private facebook: Facebook,private fb: FormBuilder) {
+    this.createForm();
   }
 
   onClick(){
     this.navCtrl.setRoot(PlayPage);
   }
-
+  createForm() {
+    this.regisForm = this.fb.group({
+      email: ['', Validators.required],
+      name : ['', Validators.required ],
+      birthday: ['', Validators.required],
+      password: ['', Validators.required],
+      password2: ['', Validators.required],
+      sex: ['', Validators.required],
+    });
+  }
+  onSubmit() {
+    if (this.regisForm.value.password === this.regisForm.value.password2) {
+      console.log('!!!!!!!!!!!!!!!!!!onSubmit - Sign Up!!!!', this.regisForm.value);
+      this.userService.register(this.regisForm.value).subscribe(res => {
+        // console.log('Res: ' + res.token);
+        localStorage.setItem('token', res.token);
+     //   this.router.navigate(['/main'], {queryParams: {token: res.token}});
+        this.navCtrl.setRoot(TabsPage);
+      }, error => {
+        console.log('Ha habido un error al registrarse:' + error);
+      });
+    } else {
+      alert('Las contraseñas no coinciden');
+    }
+  }
 
 
   signUpFacebook(){
